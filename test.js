5251b837abb5e838dd9e17474cce4d951dc8583f
Essaid08 <assaidbouhenni3@gmail.com>
2024-09-14 17:25:34 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,25 @@
+const arr = [];
+
+for (var i  = 0; i < 3; i++) {
+  const fct = () => console.log(i)
+  arr.push(fct);
+}
+
+arr.forEach((fction) => fction());
+
+
+
+
+
+const arr2 = [1 , 2, 3]
+// thes subset [[], [1] ,[2] , [3] ,[1,2] , [1,3] , [2 , 3] , [1 , 2, 3]]
+
+const subSet = (arr) => {
+  const result = []
+  const temArr = []
+  for (let i = 0; i < arr.length; i++) {
+    
+  }
+  return result
+}
+
